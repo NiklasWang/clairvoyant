a679720176bc2339d67cc565eb0f50c5a2f05d6a
liujc <jackieljc@outlook.com>
2019-09-03 19:47:44 +0800

    09/03/2019:fix git bug

---
+++ utils/FrameGenerator.d
@@ -0,0 +1,32 @@
+FrameGenerator.o FrameGenerator.d : FrameGenerator.cpp FrameGenerator.h \
+ /home/zuk/liujc10/clairvoyant/threads/ThreadT.h \
+ /home/zuk/liujc10/clairvoyant/utils/Common.h \
+ /home/zuk/liujc10/clairvoyant/PandoraInterface.h \
+ /home/zuk/liujc10/clairvoyant/Version.h \
+ /home/zuk/liujc10/clairvoyant/utils/Modules.h \
+ /home/zuk/liujc10/clairvoyant/utils/Identifier.h \
+ /home/zuk/liujc10/clairvoyant/utils/Common.h \
+ /home/zuk/liujc10/clairvoyant/log/Logs.h \
+ /home/zuk/liujc10/clairvoyant/log/LogImpl.h \
+ /home/zuk/liujc10/clairvoyant/utils/Modules.h \
+ /home/zuk/liujc10/clairvoyant/utils/Logo.h \
+ /home/zuk/liujc10/clairvoyant/utils/Semaphore.h \
+ /home/zuk/liujc10/clairvoyant/utils/QueueT.h \
+ /home/zuk/liujc10/clairvoyant/utils/clist.h \
+ /home/zuk/liujc10/clairvoyant/utils/QueueT.hpp \
+ /home/zuk/liujc10/clairvoyant/utils/QueueT.h \
+ /home/zuk/liujc10/clairvoyant/utils/SyncType.h \
+ /home/zuk/liujc10/clairvoyant/utils/Semaphore.h \
+ /home/zuk/liujc10/clairvoyant/utils/ObjectBuffer.h \
+ /home/zuk/liujc10/clairvoyant/utils/sp/RefBase.h \
+ /home/zuk/liujc10/clairvoyant/utils/sp/StrongPointer.h \
+ /home/zuk/liujc10/clairvoyant/utils/QueueC.h \
+ /home/zuk/liujc10/clairvoyant/utils/RWLock.h \
+ /home/zuk/liujc10/clairvoyant/utils/ObjectBuffer.hpp \
+ /home/zuk/liujc10/clairvoyant/utils/ObjectBuffer.h \
+ /home/zuk/liujc10/clairvoyant/utils/Atomic.h \
+ /home/zuk/liujc10/clairvoyant/utils/stdatomic.h \
+ /home/zuk/liujc10/clairvoyant/threads/ThreadT.hpp \
+ /home/zuk/liujc10/clairvoyant/memory/MemMgmt.h \
+ /home/zuk/liujc10/clairvoyant/utils/RWLock.h \
+ /home/zuk/liujc10/clairvoyant/threads/ThreadT.h
